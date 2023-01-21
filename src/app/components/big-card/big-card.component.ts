66dcb5fbc456f51bdc69868e5971c25ee025ed51
MarlonHDC <marlon.dias@ufpe.br>
2023-01-20 22:35:04 -0300

    components dinamycs prprpertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = "https://www.zup.com.br/wp-content/uploads/2021/03/5e32dc5538c14b0d4b28f87f_imagens-angular3-1024x677.png"
+  @Input()
+  cardTitle:string= ""
+  @Input()
+  cardDescription:string = ""
 
   constructor() { }
 
